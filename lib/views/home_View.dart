56139162306fb72e789be0aba3e0aa33413e6158
Riyam224 <cs50riyam211#gmail.com>
2024-03-28 02:22:13 +0300

    use list view .builder

--- lib/views/home_View.dart
+++ lib/views/home_View.dart
@@ -55,88 +55,97 @@ class _HomeViewState extends State<HomeView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        // todo
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            // todo
-            showDialog(
-              context: context,
-              builder: (BuildContext context) {
-                return Dialog(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(11)),
-                  child: Container(
-                      padding: EdgeInsets.all(10),
-                      height: 200,
-                      color: Colors.amber[200],
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          TextField(
+      // todo
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // todo
+          showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return Dialog(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(11)),
+                child: Container(
+                    padding: EdgeInsets.all(10),
+                    height: 200,
+                    color: Colors.amber[200],
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextField(
+                          // todo
+                          controller: myController,
+                          maxLength: 30,
+                          decoration: InputDecoration(
+                            hintText: 'Add new Task',
+                          ),
+                        ),
+                        SizedBox(
+                          height: 22,
+                        ),
+                        TextButton(
+                          onPressed: () {
                             // todo
-                            controller: myController,
-                            maxLength: 30,
-                            decoration: InputDecoration(
-                              hintText: 'Add new Task',
+                            addNewTask();
+                            Navigator.pop(context);
+                          },
+                          child: Text(
+                            'Add',
+                            style: TextStyle(
+                              fontSize: 20,
                             ),
                           ),
-                          SizedBox(
-                            height: 22,
-                          ),
-                          TextButton(
-                            onPressed: () {
-                              // todo
-                              addNewTask();
-                              Navigator.pop(context);
-                            },
-                            child: Text(
-                              'Add',
-                              style: TextStyle(
-                                fontSize: 20,
-                              ),
-                            ),
-                          ),
-                        ],
-                      )),
-                );
-              },
-              // todo
-            );
-          },
-          child: Icon(
-            Icons.add,
-          ),
-          backgroundColor: Colors.redAccent,
+                        ),
+                      ],
+                    )),
+              );
+            },
+            // todo
+          );
+        },
+        child: Icon(
+          Icons.add,
         ),
-        //
-        backgroundColor: Color.fromARGB(255, 8, 41, 73),
-        appBar: AppBar(
-          title: const Text(
-            'TO DO APP',
-            style: TextStyle(color: Colors.white),
-          ),
-          backgroundColor: const Color.fromARGB(255, 5, 22, 35),
+        backgroundColor: Colors.redAccent,
+      ),
+      //
+      backgroundColor: Color.fromARGB(255, 8, 41, 73),
+      appBar: AppBar(
+        title: const Text(
+          'TO DO APP',
+          style: TextStyle(color: Colors.white),
         ),
-        // todo parent container that width= double.infinity
-        body: Container(
-          margin: EdgeInsets.only(top: 20, bottom: 20),
-          width: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              // todo counter
-              CounterTasks(
-                allTodos: allTasks.length,
-                completedTasks: calculateCompletedTasks(),
+        backgroundColor: const Color.fromARGB(255, 5, 22, 35),
+      ),
+      // todo parent container that width= double.infinity
+      body: Container(
+        margin: EdgeInsets.only(top: 20, bottom: 20),
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            // todo counter
+            CounterTasks(
+              allTodos: allTasks.length,
+              completedTasks: calculateCompletedTasks(),
+            ),
+            // todo
+            Container(
+              margin: EdgeInsets.only(top: 22),
+              height: 300,
+              child: ListView.builder(
+                itemCount: allTasks.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return TodoCardWidget(
+                    todoTitle: allTasks[index].title,
+                    doneOrNot: allTasks[index].status,
+                  );
+                },
               ),
-              // todo remove [] from the list below by ...
-
-              ...allTasks
-                  .map((item) => TodoCardWidget(
-                      todoTitle: item.title, doneOrNot: item.status))
-                  .toList()
-            ],
-          ),
-        ));
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
